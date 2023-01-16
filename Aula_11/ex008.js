b8600002715cd9ee862bc307e069c09f8ecd7630
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-01-16 20:39:55 -0300

    Condições em JS

---
+++ Aula_11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 65.5
+console.log (`A velocidade do seu carro é ${vel}KM/H`)
+if (vel >  60) {
+    console.log ('Velocidade máxima permitida foi ultrapassada, MULTADO!') // Condição simples
+} 
+console.log ('Dirija sempre usando cinto de segurança')
+
---
+++ Aula_11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log (`Vivendo em ${pais}`)
+if (pais != 'Brasil') { // Conção 
+    console.log ('Você é gringo')
+} else {
+    console.log ('Você é brasileiro')
+}
