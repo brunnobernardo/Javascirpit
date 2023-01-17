1650c502607c41399ea7f4adce4a7ebbec1c7d45
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-01-16 21:50:25 -0300

    Condições aninhadas e múltiplas

--- Aula_11/ex009.js
+++ Aula_11/ex009.js
@@ -1,6 +1,6 @@
 var pais = 'EUA'
 console.log (`Vivendo em ${pais}`)
-if (pais != 'Brasil') { // Conção 
+if (pais != 'Brasil') { // Condição composta
     console.log ('Você é gringo')
 } else {
     console.log ('Você é brasileiro')
