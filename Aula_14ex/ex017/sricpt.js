bda41a2b457b5f13d12a38133e198488451053a5
brunnobernardo <105872936+brunnobernardo@users.noreply.github.com>
2023-01-21 12:51:17 -0300

    Created ex tabuada

--- Aula_14ex/ex017/sricpt.js
+++ Aula_14ex/ex017/sricpt.js
@@ -1,1 +1,21 @@
-alert('Olá')
+function calcular() {
+    let txtn = document.getElementById('txtn')
+    let numero = Number(txtn.value)
+    let tab = document.getElementById('seltab')
+    let res = document.getElementById('res')
+    res.innerHTML = `Tabuada do ${numero}`
+
+    if (txtn.value.length == 0) {
+        alert('Digite algum número para realizar a conta')
+    } else {
+        tab.innerHTML = '' // Para limpar o select antes de mostrar a próxima tabuada
+        for (let c = 1; c <= 10; c++) {
+            let calculo = c * numero // Opicional, podemos fazer essa conta direto no item.text = `${c} X ${numero} = ${c * numero}`
+            let item = document.createElement('option')
+            item.text = `${c} X ${numero} = ${calculo}`
+            item.value = `tab ${c}`
+            tab.appendChild(item)
+        }
+    }
+
+}
